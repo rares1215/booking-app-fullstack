2ed4c8d15715c4ad30d2e998d5803ab377d08ccb
Rares Giurgescu <raresgiurgescu@gmail.com>
2025-09-16 21:04:56 +0300

    first part of the front-end, just some configs for the api routing for me to be easier to fecth requests from the api

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import {Navigate} from "react-router-dom";
+import ACCESS_TOKEN, { REFRESH_TOKEN } from "../constants"
+import {jwtDecode} from "jwt-decode"
+import api from "../api"
+
+
+
+function ProtectedRoute({children}) {
+
+    const [isAuth,setIsAuth] = useState(null);
+
+    useEffect(() =>{
+        validateAuth().catch(() => setIsAuth(false))
+    },[])
+
+
+    const refreshToken = async () =>{
+        const refresh = localStorage.getItem(REFRESH_TOKEN);
+        try {
+            const res = await api.post("auth/token/refresh/", {refresh:refresh});
+            if(res.status === 200) {
+                localStorage.setItem(ACCESS_TOKEN, res.data.access);
+                setIsAuth(true)
+            }else setIsAuth(false)
+        }catch(err) {
+            console.log(err);
+            setIsAuth(false)
+        }
+    }
+
+
+    const validateAuth = async () =>{
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        if(!token) {
+            setIsAuth(false)
+            return
+        }
+        const decoder = jwtDecode(token);
+        const tokenExpDate = decoder.exp
+        const currentTime = Date.now()/1000
+
+        if(tokenExpDate < currentTime) await refreshToken();
+        else setIsAuth(true)
+    }
+
+
+    if(isAuth===null) return <div>Is Loading...</div>
+
+    return isAuth?children:<Navigate to="/login/" />
+}
+
+
+export default ProtectedRoute
